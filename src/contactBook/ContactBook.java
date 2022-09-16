0a8f463ac45363fc5e86136e5ec43a0da68ed88c
Francisco <f.silvano@campus.fct.unl.pt>
2022-09-16 22:11:17 +0100

    Boolean repeatedContacts

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -103,7 +103,7 @@ public class ContactBook {
 
         for (int i=0;i<counter;i++)
             for (int j=i+1;j<counter;j++)
-                if (i!=j && contacts[i] == contacts[j]) {
+                if (i!=j && contacts[i].getPhone() == contacts[j].getPhone()) {
                     return true;
                 }
         return false;
